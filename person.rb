26e72c7cd6b4728b38b051b65a1767c018b7258e
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-07-19 17:42:21 -0300

    Add correct_name and decorators

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
-class Person
+class Person < Nameable
   def initialize(name, age = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -8,15 +9,19 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  private
-
-  def of_age?
-    true if @age >= 18
-    false
+  def correct_name
+    @name
   end
 
   def can_use_services?
     true if is_of_age || @parent_permission
+    false
+  end
+
+  private
+
+  def of_age?
+    true if @age >= 18
     false
   end
 end
--- script.rb
+++ script.rb
@@ -1,3 +1,9 @@
-student1 = Student.new("Joseph", 4, 44)
-teacher1 = Teacher.new("Gilda", "maths", 67)
-person1 = Person.new("Ann", 17, parent_permission: false)
+student1 = Student.new('Joseph', 4, 44)
+teacher1 = Teacher.new('Gilda', 'maths', 67)
+person1 = Person.new('Ann', 17, parent_permission: false)
+person = Person.new('maximilianus', 22)
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
