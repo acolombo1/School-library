0eec92bf846a5a0d88b55ac542442058779b6db7
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-07-21 13:41:44 -0300

    Correct nameable

--- nameable.rb
+++ nameable.rb
@@ -1,8 +1,4 @@
 class Nameable
-  def initialize(nameable)
-    @nameable = nameable
-  end
-
   def correct_name
     raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
